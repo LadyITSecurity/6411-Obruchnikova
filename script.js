b78ab879312625e91a880c258d00a04488bcc6ea
Arisa <uliannaobr@gmail.com>
2022-09-15 14:28:46 +0400

    adding adaptability to elements

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ function buttonClicker() {
     let value1 = document.getElementById("value1").value;
     let value2 = document.getElementById("value2").value;
     let result = parseFloat(value1);
+
+    if (isNaN(parseFloat(value1)) || isNaN(parseFloat(value2))) {
+        result = "Ой! Вы ошиблись! Попробуйте еще раз!";
+        console.log(results);
+        document.getElementById("results").innerHTML += result + "<br/>";
+        return;
+    }
     switch (selectedElement.value) {
         case "+":
             result = parseFloat(value1) + parseFloat(value2);
@@ -26,6 +33,6 @@ function buttonClicker() {
     }
     if (result != 0)
         result = value1 + " " + selectedElement.value + " " + value2 + " " + "=" + " " + result;
-    document.getElementById("results").innerHTML += "<br/>" + result;
+    document.getElementById("results").innerHTML += result + "<br/>";
     console.log(value1, value2);
 }
